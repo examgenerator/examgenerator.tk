419f1808e1ce2dc670180bb6476b0a1509b1ea1b
examgenerator <examgenerator@marekblazek.cz>
2017-05-02 13:41:36 +0200

    upgrade to responsive

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+function navmenu () {
+    var content = document.getElementById("navcontent");
+    if (content.getAttribute("class") == "invisible")
+    {
+        content.removeAttribute("class");
+    }
+    else
+    {
+        content.setAttribute("class", "invisible");
+    }
+}
---
+++ js/webapp.js
@@ -0,0 +1,71 @@
+var numberOfQuestions;
+var text;
+var enteredString;
+var questionsEntered = new Array();
+function generate() { //generate
+    var inputString;
+    var input = new Array();
+    document.getElementById("output").value = ""; //clear output
+    inputString = document.getElementById("input").value; //create input string
+    var inputuntrimmed = inputString.split("\n") //create input Array
+    var index = 0;
+    for (x in inputuntrimmed)
+    {
+        if (inputuntrimmed[x].trim() != "")
+        {
+            input[index] = inputuntrimmed[x];
+            index += 1;
+        }
+    }
+    numberOfQuestions = parseInt(document.getElementById("numberofquestions").value); //how many questions
+    input.sort(function () {return 0.5 - Math.random()}) //randomize input Array
+    for (var i = 0; i < numberOfQuestions; i++) //write out generated test
+    {
+        if (i > input.length - 1) 
+        {
+            document.getElementById("output").value += "It was not possible to write out more questions.";
+            break;
+        }
+        else
+        {
+        text = input[i] + "\n";
+        document.getElementById("output").value += text;
+        }
+    }
+    if (document.getElementById("printver").checked == true)
+    {
+        var printwindow = window.open("");
+        printwindow.document.write("<!DOCTYPE html><html><head><title>Generated exam (examgenerator.tk)</title><style>body{font:15px Arial;}</style></head></html>")
+        for (var x = 0; x < numberOfQuestions; x++)
+        {
+            printwindow.document.write(input[x] + "<br>");
+        }
+        printwindow.document.writeln("<br>Created using examgenerator.tk");
+    }
+}
+function enteredWords() { //entered words counter
+enteredString = document.getElementById("input").value;
+questionsEntered = enteredString.split("\n");
+var length = questionsEntered.length;
+for (x in questionsEntered)
+{
+    if (questionsEntered[x].trim() == "")
+    {
+        length -= 1;       
+    }
+}
+txt = document.createTextNode(length);
+document.getElementById("entered").innerText = txt.textContent;
+if (questionsEntered[0] == '')
+{
+    txt = document.createTextNode("0");
+    document.getElementById("entered").innerText = txt.textContent;
+}
+}
+function closecookieswarning() { //cookies warning (btw there are no cookies)
+    document.getElementById("tools").removeChild(document.getElementById("cookies-warning"));
+}
+function cleario() {
+    document.getElementById("input").value = "";
+    document.getElementById("output").value = "";
+}
