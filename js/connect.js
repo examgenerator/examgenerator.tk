d5dbd8b25473f99376b7ab401a2bd94531640d64
examgenerator <examgenerator@marekblazek.cz>
2017-08-05 13:57:35 +0200

    connect update

--- js/connect.js
+++ js/connect.js
@@ -5,7 +5,7 @@ function generate() {
     definitions = document.getElementById("definitions").value.split("\n");
     definitions.sort(function () {return 0.5 - Math.random()});
     var printwindow = window.open("");
-    printwindow.document.write("<!DOCTYPE html><html><head><title>Generated exam (examgenerator.tk)</title><style>body, footer{font:15px Arial;} td{width: 200px;}</style></head><body><table>");
+    printwindow.document.write("<!DOCTYPE html><html><head><title>Generated exam (examgenerator.tk)</title><style>body, footer{font:15px Arial;} td{padding-right: 200px;}</style></head><body><table>");
     for (var x in terms) 
     {
         printwindow.document.write("<tr><td>" + terms[x] + "</td><td>" + definitions[x] + "</td><tr>");
